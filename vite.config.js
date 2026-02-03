693bdba6ff8b8f4d8d456a2a3085cfcf44b8e8d7
Hazel <hazel@local>
2026-02-03 16:59:16 +0800

    Fix: Update base path and add .nojekyll for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default {
     root: 'sources/', // Sources files (typically where index.html is)
     envDir: '../',  // Directory where the env file is located
     publicDir: '../static/', // Path from "root" to static assets (files that are served as they are)
-    base: './', // Public path (what's after the domain)
+    base: '/hazel_blog/', // Public path (what's after the domain)
     server:
     {
         // https: true,
