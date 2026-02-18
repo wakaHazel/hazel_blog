3e80344f0ac0888ee9c39ade8157407077b3f3c4
Hazel <hazel@local>
2026-02-18 23:20:10 +0800

    Docs: Add friend link to Mei You Qian's Blog

--- sources/data/social.js
+++ sources/data/social.js
@@ -1,6 +1,6 @@
 export default [
     { name: '落殷回的博客', url: 'https://luoyinhui.github.io', align: 'right' },
-    { name: '友链2', comingSoon: true, align: 'right' },
+    { name: '玫幽倩的小博客', url: 'https://mei-you-qian.github.io/', align: 'right' },
     { name: '友链3', comingSoon: true, align: 'right' },
     { name: '友链4', comingSoon: true, align: 'right' },
 ]
